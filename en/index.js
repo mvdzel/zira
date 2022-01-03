f8f5cffbe33f445da0bfe9044b3d2f5796f6e6b1
Michael van der Zel <michael.vanderzel@gmail.com>
2022-01-03 13:07:10 +0100

    Also translate Dienst

--- en/index.js
+++ en/index.js
@@ -22,6 +22,8 @@ parser.parseString(rawmax, function (err, input) {
     // Map columns of the 'Informatie' sheet to a js-struct
     const map_informatie = {
         'zira_id': 'id',
+        'informatiedomein': 'name_id_nl',
+        'informatiedomein_en': 'name_id',
         'informatieobject_en': 'name',
         'beschrijving_en': 'description'
     }
@@ -29,6 +31,8 @@ parser.parseString(rawmax, function (err, input) {
     const map_proces = {
         'type': 'type',
         'ZiRA id': 'id',
+        'Dienst': 'name_bs_nl',
+        'Dienst_en': 'name_bs',
         'Bedrijfsproces_en': 'name_bp',
         'Werkproces_en': 'name_wp',
         'BA / Processtap_en': 'name_ba',
@@ -60,6 +64,15 @@ parser.parseString(rawmax, function (err, input) {
             object["alias"] = row.name;
             object["notes"] = '<languages xml:space="preserve"><nl-NL>' + object["notes"] + '</nl-NL><en-US>' + row.description + '</en-US></languages>';
             output['model'].objects.object.push(object);
+
+            // add Informatie Domein vertaling through the name and type == 'Grouping', ID for Informatie Domain are missing
+            // var object_id = input['model'].objects[0].object.find(element => element.name == row.name_id_nl);
+            // if (object_id.stereotype == "ArchiMate_Grouping" && !output['model'].objects.object.find(element => element.id == object_id.id)) {
+            //     console.error(object_id);
+            //     delete object_id["modified"];
+            //     object_id["alias"] = object_id;
+            //     output['model'].objects.object.push(object_id);            
+            // }
         });
 
         readXlsxFile(filename_xlsx_en, { sheet: "Proces", map: map_proces }).then(( {rows, errors}) => {
@@ -73,6 +86,14 @@ parser.parseString(rawmax, function (err, input) {
                 }
                 object["notes"] = '<languages xml:space="preserve"><nl-NL>' + object["notes"] + '</nl-NL><en-US>' + row.description + '</en-US></languages>';
                 output['model'].objects.object.push(object);
+
+                // if this is a BP row then also translate the BS through the name, ID for BS are missing
+                if (row.type == "BP") {
+                    var object_bs = input['model'].objects[0].object.find(element => element.name == row.name_bs_nl);
+                    delete object_bs["modified"];
+                    object_bs["alias"] = row.name_bs;
+                    output['model'].objects.object.push(object_bs);
+                }
             });
 
             readXlsxFile(filename_xlsx_en, { sheet: "BedrijfsFuncties", map: map_bf }).then(( {rows, errors}) => {
